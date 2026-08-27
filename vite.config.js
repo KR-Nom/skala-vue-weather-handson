e1fd67bde825db307083a46d669fd6a3efee73b8
Jang Hyeonjin <KR-Nom@users.noreply.github.com>
2026-08-27 17:02:27 +0900

    feat: complete weather deployment assignment

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
